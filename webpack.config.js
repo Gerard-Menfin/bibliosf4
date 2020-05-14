2f869584b92869ab8409f39695ed01eb2d115a9e
Gerard-Menfin <gerard.mentor314@gmail.com>
2020-05-15 00:21:27 +0200

    CSS admin

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ Encore
      */
     .addEntry('app', './assets/js/app.js')
     .addEntry('device', './assets/css/device.css')
+    .addEntry('admin', './assets/css/admin.scss')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
