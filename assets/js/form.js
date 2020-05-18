79b7f609bdda6558af41775155a24f7a481141c8
Gerard-Menfin <gerard.mentor314@gmail.com>
2020-05-19 00:32:07 +0200

    form.js : amélioration

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,10 +1,10 @@
 $(function(){
     // ⚠ : il faut utiliser 'prop' plutôt que 'attr'
-    var selecteur = "[name='abonne[roles][]']";
-    selecteur = ".unique :checkbox";
+    // var selecteur = "[name='abonne[roles][]']";
+    var selecteur = ".unique :checkbox";
     $(selecteur).change(function() {
         console.log("checkbox change:" + selecteur);
-        $(selecteur).prop("checked", false);
+        $(selecteur).parent().find(":checkbox").prop("checked", false);
         $(this).prop("checked", true);
     });
 });
