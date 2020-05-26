00460d1890a07c673b17fa356104fce4817286f3
Gerard-Menfin <gerad.mentor314@gmail.com>
2020-05-26 20:48:10 +0200

    Repository - surcharge findAll  : tri possible

--- src/Repository/Depot.php
+++ src/Repository/Depot.php
@@ -7,7 +7,7 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /*
-    Toutes les classes Repository vont hériter de cette classe Depot
+    Toutes les classes Repository devront hériter de cette classe Depot
     pour avoir des méthodes génériques
 */
 abstract class Depot extends ServiceEntityRepository
@@ -25,4 +25,51 @@ abstract class Depot extends ServiceEntityRepository
                     ;
         return $resultat ? $resultat["nb"] : 0;
     }
+
+    /**
+     * La méthode findAll de la classe Doctrine\ORM\EntityRepository appelle $this->findBy([])
+     * et donc ne peut pas être triée. On la redéfinie (= surcharge) dans la classe dont tous 
+     * les Repository vont hériter : 
+     *
+     * @param array|null $orderBy
+     * 
+     * @return array d'entités.
+     */
+    public function findAll($orderBy = null)
+    {
+        return $this->findBy([], $orderBy);
+    }
+
+
+
+
+    // /**
+    //  * @return Livre[] Returns an array of Livre objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('l.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Livre
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+
 }
